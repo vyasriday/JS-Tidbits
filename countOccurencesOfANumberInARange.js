516fcdbb5a41c765c83d5f88e3e416e61e31d22f
Hridayesh Sharma <vyasriday7@gmail.com>
2019-07-11 19:18:17 +0530

    added optimized solution

--- countOccurencesOfANumberInARange.js
+++ countOccurencesOfANumberInARange.js
@@ -1,3 +1,7 @@
+/*
+  The follwing functions takes two arguments and returns the number of times the passed number occues in that range. 
+  For example between 1 to 27 the number 7 occues 3 times. Once in 7 then in 17 and third time in 27 
+*/
 function countOccurences(lower, higher, number) {
   counter = 0
   for(var iteratedNumber = lower; iteratedNumber<= higher; iteratedNumber++){
@@ -9,6 +13,17 @@ function countOccurences(lower, higher, number) {
           count++	
       })
       counter += count
+    }
+  }
+  return counter;
+}
+
+// optimized solutiom
+function countOccurencesOptimized(lower, higher, number) {
+  let counter = 0
+  for(var iteratedNumber = lower; iteratedNumber<= higher; iteratedNumber++){
+        if(iteratedNumber.toString().includes(number)) {
+        	counter += iteratedNumber.toString().split('number').length - 1
     }
   }
   return counter;
