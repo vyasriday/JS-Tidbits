d1741a26650d5be910a014e75280b6009b80271f
Hridayesh Sharma <vyasriday7@gmail.com>
2019-07-14 16:37:36 +0530

    function to find longest distinct substring in a string

---
+++ src/js-interview-questions/longestDistinctSubstring.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+function findLongestUniqueSubstring(string) {
+  let substringsArray = [];
+  for (let i = 0; i < string.length; i++) {
+    let substring = '';
+    for (let j = i; j < string.length; j++) {
+      if (substring.includes(string[j])) {
+        break;
+      } else {
+        substring += string[j];
+      }
+    }
+    substringsArray.push(substring);
+  }
+  return substringsArray.sort((a, b) => b.length - a.length)[0];
+}
+
+findLongestUniqueSubstring('abbbccddeeabcdefghijk');
