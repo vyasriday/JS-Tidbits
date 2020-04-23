e3dfcba9798c1ce2eee65999c722dc362bfe8dd5
Hridayesh Sharma <vyasriday7@gmail.com>
2020-04-23 21:25:57 +0530

    added es7 features

---
+++ src/js-concepts/ES7.js
@@ -0,0 +1,9 @@
+// includes on arrays and strings
+
+[1,2,3].includes(1,0); // check if array includes 1 starting from index 0
+
+"Hello".includes('H', 1); // false
+
+// Power Operator
+
+2**3 // 8
