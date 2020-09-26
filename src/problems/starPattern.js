f5d3e22ed3fe286fd17c8e6e6ca197db5d50afaa
Reynadi Andreas Priesley <43875921+Reynadi531@users.noreply.github.com>
2020-09-26 20:02:05 +0700

    Simplify StarPattern solution (#19)

--- src/problems/starPattern.js
+++ src/problems/starPattern.js
@@ -2,6 +2,7 @@
 for a given value of n, print a star pattern starting from 1 star each line to n stars in the last line
 */
 
+// Method 1 - Using for loop
 const starPattern = (n) => {
     for(let i = 1; i <= n; i++) {
         let pattern = "";
@@ -13,7 +14,15 @@ const starPattern = (n) => {
     }
 }
 
+// Method 2 - Using for loop and repeat function
+const starPatternWithRepeat = (n) => {
+	for(let i=1; i<=n; i++) {
+		console.log("* ".repeat(i));
+	} 
+}
+
 starPattern(5);
+//starPatternWithRepeat(5);
 
 /*
 * 
