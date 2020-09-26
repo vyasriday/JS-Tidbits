f109238c9b684c2d7cd59ab4f92785a7836aa8ee
RAYARK\jimhsu <jimhsu@jet-gen.com>
2020-09-26 17:23:51 +0800

    Add alternate solutions for printStar

--- problems/printStar.js
+++ problems/printStar.js
@@ -8,10 +8,18 @@
     *****
  */
 
-printStar = (value) => {
+// Method 1 - Using a simple for loop and string Repeat
+printStarRepeat = (value) => {
     for(let i = 1; i <= value; ++i)
     {
         console.log('*'.repeat(i));
     }
   };
   
+// Method 2 - Using a simple for loop and Instantiate new array fill with various count of star signs then combime them
+printStarArrayFill = (value) => {
+    for(let i = 1; i <= value; ++i)
+    {
+        console.log(Array(i).fill('*').join(''));
+    }
+  };
