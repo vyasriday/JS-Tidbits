58e5ab17b18e9a7f66f34bc863be2629742bbc2d
RAYARK\jimhsu <jimhsu@jet-gen.com>
2020-09-26 11:53:59 +0800

    Add printStar.js for issue #16

---
+++ src/problems/printStar.js
@@ -0,0 +1,17 @@
+/**
+ * for a number n prints pyramid of star isgns
+ * for example printStar(5) ->
+    *
+    **
+    ***
+    ****
+    *****
+ */
+
+printStar = (value) => {
+    for(let i = 1; i <= value; ++i)
+    {
+        console.log(`${Array(i).fill('*').join('')}`);
+    }
+  };
+  
