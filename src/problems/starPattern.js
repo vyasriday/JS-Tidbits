5774c3ae5fd3a0e627cc3a91c749a6bfe7502a69
Reynadi Andreas Priesley <43875921+Reynadi531@users.noreply.github.com>
2020-09-26 13:15:01 +0700

    Simplify StarPattern solution
    using repeat function instead of using array or other solution. More info https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat

--- src/problems/starPattern.js
+++ src/problems/starPattern.js
@@ -3,17 +3,15 @@ for a given value of n, print a star pattern starting from 1 star each line to n
 */
 
 const starPattern = (n) => {
-    for(let i = 1; i <= n; i++) {
-        let pattern = "";
-        for(let j = 1; j <= i; j++) {
-            pattern += "* ";
-        }
-
-        console.log(pattern);
+    for(let i=1; i<=n; i++){ 
+        console.log("*".repeat(i)); // Repeat "*" by i
     }
 }
 
 starPattern(5);
+
+// Docs about repeat function
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 
 /*
 * 
