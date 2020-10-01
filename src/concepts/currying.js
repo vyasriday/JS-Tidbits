bd649ee4b726fd349027a73d49e527a9e8baf4aa
Aman Agrawal <amanagrawal.am@gmail.com>
2020-10-01 20:40:59 +0530

    Added advanced currying implementation (#22)

--- src/concepts/currying.js
+++ src/concepts/currying.js
@@ -12,3 +12,32 @@ currying(2)(3)(4);
 
 const es6Currying = (x) => (y) => (z) => x+y+z;
 es6Currying(2)(3)(4);
+
+
+// Adding advanced currying implementation, create a function arguments into currying 
+
+
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
+  };
+}
+
+
+// example 
+
+function multiply(a, b, c) {
+  return a * b * c;
+}
+
+let curriedMul = curry(multiply);
+
+console.log(curriedMul(1, 2, 3) ); // 6, still callable normally
+console.log(curriedMul(1)(2,3) ); // 6, currying of 1st arg
+console.log(curriedMul(1)(2)(3) ); // 6, full currying
