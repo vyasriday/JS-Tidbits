0265bc084b876fc9c6d678d81724e69711795fed
Hridayesh Sharma <dev.hridayesh@gmail.com>
2022-09-12 19:56:38 +0530

    Update currying.js

--- src/concepts/currying.js
+++ src/concepts/currying.js
@@ -41,3 +41,24 @@ let curriedMul = curry(multiply);
 console.log(curriedMul(1, 2, 3) ); // 6, still callable normally
 console.log(curriedMul(1)(2,3) ); // 6, currying of 1st arg
 console.log(curriedMul(1)(2)(3) ); // 6, full currying
+
+
+
+// infinite currying
+// Implement currying sum(1)(2)() => 3, sum(1,2)(3,4)(5)(6,7,8)() => 34
+function sum(...args) {
+ if(args.length === 0) {
+	 return this.total;
+ }
+
+ return (function(self) {
+	 if(self.total) {
+		 self.total +=  args.reduce((total, current) => total+current,0);
+	 } else {
+		 self = {total: args.reduce((total, current) => total+current,0) }
+
+	 }
+
+	 return sum.bind(self);
+ })(this);
+}
